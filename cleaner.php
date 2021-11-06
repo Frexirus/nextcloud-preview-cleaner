d3429f032cfd73fe370b7fc3fe61b89f41801d4f
Frexirus <admin@frexirus.ru>
2021-11-07 02:36:06 +0300

    Update cleaner.php

--- cleaner.php
+++ cleaner.php
@@ -1,57 +1,54 @@
 <?php
-//Не забудте установить на данный скрипт привилегии выполнения от имени вебсервера
-//Добавление задачи -> crontab -e -u www-data
-//*  */12  *  *  * php -f /var/www/nextcloud/cleaner.php > /dev/null 2>&1
-//*  */12  *  *  * php -f /var/www/nextcloud/occ files:scan-app-data > /dev/null 2>&1
-
-$key = 'TqLHu47GZVGIFor4'; //Ключ входа
-
-//if($key != $_GET['key']){echo "OOPS"; exit();}
+/*
+https://github.com/Frexirus/nextcloud-preview-cleaner
+*/
 
 require_once __DIR__ . '/config/config.php'; //Подключение конфига некстклауда
 
 $pdo = new PDO('pgsql:host='.$CONFIG['dbhost'].';dbname='.$CONFIG['dbname'],$CONFIG['dbuser'],$CONFIG['dbpassword']);
-//Список всех строк по столбцу fileid
-$sql = "SELECT fileid FROM oc_filecache";
+//Список строк связанных с изображениями по столбцу fileid
+$sql = "SELECT fileid FROM oc_filecache WHERE path NOT LIKE '%preview%' AND mimepart = 3";
 foreach ($pdo->query($sql) as $row) {
-	$column_fileid[]=$row['fileid'];
+	$column_fileid_users_images[]=$row['fileid'];
 }
-//Отфильтрованный список по частичному совпадению path и mimepart (3 <- images)
-$sql = "SELECT name FROM oc_filecache WHERE path LIKE '%preview%' AND mimepart = 3";
+//Поиск парентов изображений в папке превью
+$sql = "SELECT parent FROM oc_filecache WHERE path LIKE '%preview%' AND mimepart = 3";
 foreach ($pdo->query($sql) as $row) {
-	$column_name_pm[]=$row['name'];
+	$column_name_preview_parents[]=$row['parent'];
 }
-//Собираем список парентов кэша
-for ($i = 0; $i <= count($column_name_pm)-1; $i++) {
-	$sql = "SELECT parent FROM oc_filecache WHERE name = '$column_name_pm[$i]'";
+$column_name_preview_parents = array_values(array_unique($column_name_preview_parents));
+for ($i = 0; $i <= count($column_name_preview_parents)-1; $i++) {
+	$sql = "SELECT name FROM oc_filecache WHERE fileid = $column_name_preview_parents[$i]";
 	foreach ($pdo->query($sql) as $row) {
-		$column_parent[]=$row['parent'];
+		$column_fileid_preview_images[]=$row['name'];
 	}
 }
-//Создаём список имён папок кэша картинок
-for ($i = 0; $i <= count($column_parent)-1; $i++) {
-	$sql = "SELECT path,name FROM oc_filecache WHERE fileid = '$column_parent[$i]' AND mimepart = 1";
+//Сравнение списков и удаление из него используемых превью
+$column_fileid_preview_unused_images = array_values(array_diff($column_fileid_preview_images,$column_fileid_users_images));
+
+//Собираем пути и паренты неиспользуемого кэша
+for ($i = 0; $i <= count($column_fileid_preview_unused_images)-1; $i++) {
+	$sql = "SELECT fileid,path,parent FROM oc_filecache WHERE name = '$column_fileid_preview_unused_images[$i]' AND mimepart = 1";
 	foreach ($pdo->query($sql) as $row) {
-		$column_name_folders[]=$row['name'];
+		$column_fileid_unused_rows[]=$row['fileid'];
+		$column_path_unused_rows[]=$row['path'];
+		$column_parent_unused_rows[]=$row['parent'];
 	}
 }
-$unused_dirs = array_values(array_unique(array_diff($column_name_folders, $column_fileid)));
-//Собираем пути неиспользуемого кэша
-for ($i = 0; $i <= count($unused_dirs)-1; $i++) {
-	$sql = "SELECT path FROM oc_filecache WHERE name = '$unused_dirs[$i]' AND mimepart = 1";
-	foreach ($pdo->query($sql) as $row) {
-		$column_path_folders[]=$row['path'];
-	}
-}
-//Удаляем список ненужного
-foreach ($column_path_folders as $row) {
+//Чистим мусор
+foreach ($column_path_unused_rows as $i=>$row) {
 	if ($row != "." and $row != "..") {
 		$directory = $CONFIG['datadirectory']."/".$row;
 		foreach (glob($directory."/*.*") as $filename) {
 			unlink ($filename);
 		}
 		if (rmdir($directory)) {
-			echo "REMOVED -> ".$directory."<br />";
+			echo "REMOVED FOLDER -> ".$directory."<br />";
+			$sql = "DELETE FROM oc_filecache WHERE parent = $column_parent_unused_rows[$i]";
+			if ($pdo->query($sql)) {
+				$sql = "DELETE FROM oc_filecache WHERE parent = $column_fileid_unused_rows[$i]";
+				$pdo->query($sql);
+			}
 		}
 		else
 		{
